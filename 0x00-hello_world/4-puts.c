0dedc96823e0c6f34b5cb8704c87ddc47ef80861
zouhairmrt <marioutezohair@gmail.com>
2023-10-05 11:28:57 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - À program that prints a line with puts function
  * Return: 0 (Success)
- * /
+ */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0):
 }
