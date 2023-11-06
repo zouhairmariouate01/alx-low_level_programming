31e84377494513ceb2fbba13e5f9d501c2a758a8
zouhairmrt <marioutezohair@gmail.com>
2023-11-06 02:50:42 -0800

    0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -29,3 +29,45 @@ char *_strcpy(char *dest, char *src)
 {
 	char *ptr = dest;
 
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (ptr);
+}
+/**
+ * new_dog - creates a new dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Return: a pointer to the new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	_strcpy(new_dog->name, name);
+	_strcpy(new_dog->owner, owner);
+	new_dog->age = age;
+	return (new_dog);
+}
