7037c3f3aea371732a017fe0f383ef9633d6137d
zouhairmrt <marioutezohair@gmail.com>
2023-11-16 14:23:04 -0800

    aal

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,8 +7,8 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
  * Description: singly linked list node structure
+ * for alx
  */
 typedef struct list_s
 {
