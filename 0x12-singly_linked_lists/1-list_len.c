4fd9e33eea99b3f2cfb4e80761ea671d9d93dd6d
zouhairmrt <marioutezohair@gmail.com>
2023-11-16 15:35:25 -0800

    aaal

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - function that counts the number of elements in a list_t list
+ * @h: pointer to the list_t structure to count
+ *
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+    size_t count = 0;
+
+    while (h != NULL)
+    {
+        count++;
+        h = h->next;
+    }
+    return (count);
+}
