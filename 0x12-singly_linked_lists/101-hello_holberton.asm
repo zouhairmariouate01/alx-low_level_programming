6cf7ad3d94216134fa2e63c86cf4f3321a2b6938
zouhairmrt <marioutezohair@gmail.com>
2023-11-16 15:57:59 -0800

    lla

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,25 @@
 section .data
-msg db "Hello, Holberton", 0xA ; C string to be printed
+msg db "Hello, Holberton", 0xA ; '0xA' stands for new line
+len equ $- msg                 ; Calculate length of message
 
 section .text
 global main
-extern printf ; we are using the C function printf
+extern printf                   ; Import the C function printf
 
 main:
-    ; we push the message to the stack
-    ; because printf requires the parameters in a certain order
-    push rbp ; push base pointer onto the stack
-    mov rdi, msg ; load address of message into rdi
-    call printf ; call C function
+    push rbp                    ; Set up stack frame
+    mov rbp, rsp
+    sub rsp, 16                 ; Allocate space for locals (none in this program)
 
-    ; exit the program
-    pop rbp ; pop the base pointer off the stack
-    mov rax, 60 ; 60 is the syscall number for exit on x86_64
-    xor rdi, rdi ; set rdi to 0
-    syscall ; call the kernel
+    ; Push the parameters in reverse order
+    push len
+    push msg
+    call printf                 ; Call the C function printf
+
+    ; Clean up the stack frame
+    mov rsp, rbp
+    pop rbp
+
+    ; Return 0 (success)
+    xor rax, rax
+    ret
