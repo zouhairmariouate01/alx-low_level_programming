d993fc368f54a92686141bf946da3943d22501d2
zouhairmrt <marioutezohair@gmail.com>
2023-11-23 16:18:22 -0800

    a

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,6 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-    *n = *n | (1 << index);
+     *n |= (1UL << index);
+
     return (1);
 }
