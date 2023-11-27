775b1903b1277a4427c2227a5bc4ff71e496e578
zouhairmrt <marioutezohair@gmail.com>
2023-11-27 12:29:51 -0800

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,39 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
- * Return: the actual number of letters it could read and print
- * If the file can not be opened or read, return 0
- * If filename is NULL return 0
- * If write fails or does not write the expected amount of bytes, return 0
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t n, read_count = 0;
-	char buffer[letters + 1];
+	ssize_t o, r, w;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	while (read_count < letters)
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		n = read(fd, buffer, letters - read_count);
-		if (n == -1)
-		{
-			close(fd);
-			return (0);
-		}
-
-		buffer[n] = '\0';
-
-		if (write(1, buffer, n) != n)
-		{
-			close(fd);
-			return (0);
-		}
-
-		read_count += n;
+		free(buffer);
+		return (0);
 	}
 
-	close(fd);
-	return (read_count);
+	free(buffer);
+	close(o);
+
+	return (w);
 }
