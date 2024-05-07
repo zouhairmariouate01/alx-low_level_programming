b69a6ed6072f822da8e9fe853000cfdf347dbd72
zouhairmariouate01 <zouhairmarioute@gmail.com>
2024-05-07 14:44:15 -0700

    task

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,21 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - searches for a value in an array of
+ * integers using the Interpolation search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t pos, low, high;
+	double f;
+
+	if (array == NULL)
+		return (-1);
+
+	low = 0;
+
