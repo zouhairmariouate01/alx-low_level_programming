f463df7c8b8b35a202fdd13b8072724c9d187f48
zouhairmariouate01 <zouhairmarioute@gmail.com>
2024-05-08 13:29:51 -0700

    ttask

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -30,4 +30,22 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		while (list->next && list->index < index)
 			list = list->next;
 
+		if (list->next == NULL && index != list->index)
+			index = list->index;
 
+		printf("Value checked at index [%d] = [%d]\n", (int)index, list->n);
+
+	} while (index < size && list->next && list->n < value);
+
+	printf("Value found between indexes ");
+	printf("[%d] and [%d]\n", (int)prev->index, (int)list->index);
+
+	for (; prev && prev->index <= list->index; prev = prev->next)
+	{
+		printf("Value checked at index [%d] = [%d]\n", (int)prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
+	}
+
+	return (NULL);
+}
